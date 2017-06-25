94588099ac7c0096d94d03f6c7231a46d8872f89
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-06-24 22:01:07 -0400

    Installed Gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by apoorvmittal on 6/24/17.
+ */
+var gulp = require('gulp'),
+     gutil = require('gulp-util');
+
+gulp.task('log', function () {
+    gutil.log('You are using Gulp');
+});
