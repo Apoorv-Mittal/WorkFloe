315714ebfe50b3fc44a2b1227d18bd0890c17715
Apoorv Mittal <mittal.apoorv2012@gmail.com>
2017-06-25 16:59:41 -0400

    minified html

--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,83 @@
  */
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat');
-    browserify = require('gulp-browserify');
-    connect = require('gulp-connect');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    pngcrush = require('gulp-pngcrush'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('log', function () {
     gutil.log('You are using Gulp');
 });
 
+var env,
+    jsSources,
+    htmlSources,
+    jsonSources,
+    outputDir;
+
+
+env = process.env.NODE_ENV  || 'development';
+
+if( env ==='development'){
+    outputDir = 'builds/development/'
+}else {
+    outputDir = 'builds/production/'
+}
+
+
 // add all the js file names in here
-var jsSources =[
-    'components/scripts/template.js'
-];
-var htmlSources=['builds/development/*.html'];
-var jsonSources=['builds/development/*.json'];
+jsSources =['components/scripts/template.js'];
+htmlSources=[outputDir +'*.html'];
+jsonSources=[outputDir+'*.json'];
 
 
 gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'))
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir+'js'))
         .pipe(connect.reload())
 });
 
 //can add more tasks that monitors any change and then reruns the task if there is an error
 gulp.task('watch', function () {
     gulp.watch(jsSources, ['js']);
-    gulp.watch(htmlSources,['html']);
+    gulp.watch('builds/development/*.html',['html']);
     gulp.watch(jsonSources,['json']);
+    gulp.watch(outputDir+'images/**/*.*',['images']);
 });
 
 gulp.task('connect', function () {
     connect.server({
-        root :'build/development/',
+        root :outputDir,
         livereload: true
     });
 });
 
 //looks for change in the html file
 gulp.task('html', function () {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production',minifyHTML()))
+        .pipe(gulpif(env === 'production',gulp.dest(outputDir)))
+        .pipe(connect.reload())
+});
+
+
+//crushes images
+gulp.task('images', function () {
+    gulp.src('builds/development/images/**/*.*')
+        .pipe(gulpif(env === 'production',imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngcrush()]
+        }))
+        .pipe(gulpif(env === 'production',gulp.dest(outputDir + 'images'))))
         .pipe(connect.reload())
 });
 
@@ -53,4 +89,4 @@ gulp.task('json', function () {
         .pipe(connect.reload())
 });
 //in the array all the gulp tasks
-gulp.task('default',['html','js','json','watch','connect']);
+gulp.task('default',['html','js','json','images','watch','connect']);
